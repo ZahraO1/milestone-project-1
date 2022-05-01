79f572e96c9cf9e186b678429bd5586dc91e6324
ZahraO1 <zahra.masroor6@gmail.com>
2022-05-01 10:41:11 -0400

    added function to change card locations when clicked

--- main.js
+++ main.js
@@ -74,6 +74,7 @@ function reset(){
     document.getElementById("player-2-value").textContent = ""
     document.getElementById("player-3-value").textContent = ""
     document.getElementById("player-4-value").textContent = ""
+    createStart()
 }
 
 //if a player is said to go fish, they draw
@@ -105,7 +106,7 @@ function start(){
     updateDecks()
     createReset()
 }
-
+let v =0;
 function updateDecks(){
     document.getElementById("deck").textContent = cards.length
 
@@ -124,8 +125,9 @@ function updateDecks(){
         player1cards[i].style.top = `${y}%`;
         player1cards[i].style.display = "block";
     }
-
 }
+
+
 var player1cards = []
 function cardsArray(){
     player1cards[0] = document.getElementById("card-1")
@@ -158,7 +160,59 @@ function createReset(){
 }
 
 function createStart(){
-    
+    var deck = document.getElementById("deck")
+    var startButton = document.createElement('button')
+    startButton.textContent = "Start"
+    startButton.style.backgroundColor = "blue";
+    startButton.style.color = 'white'
+    startButton.style.position = 'absolute'
+    startButton.style.top = '70%'
+    startButton.style.left = '25%'
+    startButton.style.width = '50%'
+    startButton.style.height = '15%'
+    deck.appendChild(startButton)
+    startButton.addEventListener('click',start)
+}
+
+let playerTurn = 0
+function turns(){
+    let turnStart = 1
+    //after each turn, add one to playerTurn for which player is going
+    //when playerTurn equals 3, set the playerTurn value into 0 so it repeats
+    //maybe have a promise so that when PLayer 4 is done, it goes to player one
+    //when turnStart equal 0, then the game ends
+    while(turnStart == 1){
+        switch (playerTurn){
+            case 0:
+                break;
+            case 1:
+                AITurn(playerTurn)
+                break;
+            case 2:
+                AITurn(playerTurn)
+                break;
+            case 3:
+                AITurn(playerTurn)
+                break;
+            case 4:
+                playerTurn = 0;
+                break;
+        }
+        playerTurn++;
+    }
+}
+
+function cardsClick(n){
+    player1cards[n].style.transform = `translate(${160+((n-1)*46)}%)`;
+    //this for player 4 translation
+    //need the translation to go to the player's show cards placement
+    setTimeout(()=>{player1cards[n].style.transform = `translate(${400+((n-1)*46)}%,-215%)`;},1000);
+}
+
+function AITurn(opponent){
+    //when it's their turn
+    //check more of which cards they have
+    //if each 
 }
 
 
@@ -176,4 +230,5 @@ function createStart(){
 
 //need a function for turns and timing
 //==> since there are 4 players, index 0,1,2,3
-//
+
+//how does the user know who is asking for what
